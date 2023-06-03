b1be85437c5e403822b0479ba7f0531e52128b26
sdsartor <sdsartor48@gmail.com>
2023-06-03 14:48:42 -0700

    updated credits page

--- client/src/pages/Credits.js
+++ client/src/pages/Credits.js
@@ -6,39 +6,39 @@ const Credits = () => {
         <>
        <div></div>
       <div className="card">
-  <div className="content">
+  <div className="content" style={{marginLeft:"700px", marginTop: "200px"}}>
     <div className="front" id='hunter'>
     </div>
     <div id="back0">
-      Back!
+      Commander: Stephen
     </div>
   </div>
 </div>
 <div className="card">
-  <div className="content" style={{marginTop:"600px"}}>
+  <div className="content" style={{marginTop:"800px", marginLeft:"800px"}}>
     <div className="front" id='warrior'>
     </div>
-    <div id="back1" style={{marginBottom:"400px"}}>
-      Back!
+    <div id="back1" style={{}}>
+     Commander: Matthew
     </div>
   </div>
 </div>
 
-<div className="card" style={{marginLeft:"0px"}}>
-  <div className="content">
+<div className="card" style={{marginLeft:"900px"}}>
+  <div className="content" style={{marginTop: "200px"}}>
     <div className="front" id='astro'>
     </div>
     <div id="back2">
-      Back!
+      Commander: Dillon
     </div>
   </div>
 </div>
-<div className="card" style={{marginLeft:"0px"}}>
-  <div className="content" style={{marginLeft:"910px",marginTop:"-100px"}}>
-    <div className="front" id='hustler' style={{marginLeft:"30px",marginTop:"0px"}}>
+<div className="card" style={{}}>
+  <div className="content" style={{marginLeft:"100px", marginTop: "200px"}}>
+    <div className="front" id='hustler' >
     </div>
     <div id="back3">
-      Back!
+      Commander: James
     </div>
   </div>
 </div>
