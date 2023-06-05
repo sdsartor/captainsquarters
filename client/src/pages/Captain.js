9c2be787ae122c51616bad657d83121d9083afef
bsidemyself <shackletron@gmail.com>
2023-06-04 21:34:25 -0700

    added icons and  font

--- client/src/pages/Captain.js
+++ client/src/pages/Captain.js
@@ -4,6 +4,7 @@ import { useMutation } from '@apollo/client';
 import { CREATE_CAPTAIN } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+import { CaptCreation } from '../components/CaptCreation';
 
 let Captain
 export default Captain;
